47c21dbb64b601f79059cdae3cac51a38080184c
Matheus Mizobe <ricardomizobe303@gmail.com>
2024-08-30 06:31:21 +0000

    03

---
+++ atividade03/script.js
@@ -0,0 +1,33 @@
+const Dados = [];
+
+function atualizarLista(){
+    Dados.sort((a,b) =>a.nome.localeCompare(b.nome));
+    
+    const Lista = document.getElementById("list");
+    Lista.innerHTML = '';
+
+    Dados.forEach(item => {
+        const li = document.createElement('li');
+        li.textContent = `${item.nome} - ${item.idade} anos`;
+        Lista.appendChild(li);
+    });
+}
+
+document.getElementById('Formulario').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const ADDnome = document.getElementById('nome');
+    const ADDidade = document.getElementById('idade');
+    const nome = ADDnome.value.trim();
+    const idade = parseInt(ADDidade.value.trim(), 10);
+
+    if(nome && isNaN(idade)){
+        Dados.push({nome, idade});
+        atualizarLista();
+
+        ADDnome.value = '';
+        ADDidade.value = '';
+
+
+    }
+});
